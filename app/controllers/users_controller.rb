e77d1e36d5031ee6160524c966735adf0a3a035e
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2020-01-11 01:29:17 +0000

    form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find(params[:id])
+    if @user.save
+    else
+    end
+  end
+end
