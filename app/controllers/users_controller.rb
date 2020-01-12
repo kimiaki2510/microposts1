c5b49239a122a4f4de1c579db87711dcd52c2fb8
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2020-01-12 01:25:42 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,38 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:id])
+    @user = User.new(user_params)
+
     if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
     else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
     end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def login(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+    else
+      return false
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
