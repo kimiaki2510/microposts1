3e2ae16f2468c2dca413f8f6a774d8393f45b0cf
kimiaki.sugiyama <kimiaki.sugiyama@techacademy.jp>
2020-01-14 13:58:29 +0000

    follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -27,13 +27,16 @@ class UsersController < ApplicationController
     end
   end
   
-  def edit
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
   end
   
-  def update
-  end
-  
-  def destroy
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
   
   private
